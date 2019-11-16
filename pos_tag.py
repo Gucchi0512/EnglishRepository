0b1e8786279bf50f27325c46aa41cd90cbe1a9e8
Kuayato <aizu.u.transfer@gmail.com>
2019-11-16 13:50:46 +0900

    messge

---
+++ pos_tag.py
@@ -0,0 +1,15 @@
+import nltk
+def test():
+    sentence = "I have been waiting for him since last night."
+    #sentence = input()
+    pos_tag(sentence)
+
+def pos_tag(sentence):
+    t = nltk.word_tokenize(sentence)
+    pos = nltk.pos_tag(t)
+    print(pos)
+    return(pos)
+
+def
+
+test()
